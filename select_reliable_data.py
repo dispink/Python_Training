ed9a3b69e2f7496b2aa2b31748db56bddc2a2699
dispink <r04224110@ntu.edu.tw>
2018-09-13 16:03:43 +0800

    "select_reliable_data.py" is a tool developed from "select_data_by_FeAr.py" and  based on the conclusion of "select_reliable_data_workflow.py". It selects the data whose Ar/Fe value is lower than one standard deviation of overall Ar/Fe, MSE is lower than 1.5, and validity is 1. Besides, the last 10 data points are excluded due to the possible miss-scanning on tape. The selected data are output as "refine.csv". The excluded data are output as "deleted.csv".

--- select_reliable_data.py
+++ select_reliable_data.py
@@ -1,15 +1,17 @@
-#input_name = input()					#是讀取已經covert成csv檔的檔案
+# this script is based on the conclusion of select_reliable_data_workflow.py
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 input_file = os.path.expanduser('~/Documents/Python_Training/out.csv')	#python並不會自動展開~/的路徑，所以需要這個指令來展開
 with open(input_file, 'r') as f:		#with...as...建立一個區塊中的某指令簡寫，當程式碼離開這個區塊(以對線做區隔)，便自動取消著個指令動作 
 	data = []							#故也可做為自動close檔案的一個好方法
 	for index, line in enumerate(f):
 		if index == 0:
-			column_info = line[:-1].split(',')
+			column_info = line[:-1].split(',')       #enumerate讀入檔案會在最後加上\n，所以須拍除這一項
 		else:
 			data = data + [line[:-1].split(',')]
+
 for index, value in enumerate(column_info):
     if value == 'Ar':
         ar_col = index
@@ -17,23 +19,39 @@ for index, value in enumerate(column_info):
         fe_col = index
     if value == 'MSE':
         mse_col = index
+    if value == 'validity':
+        vld_col = index
+
 
 ratio = []
 mse = []
+
 for datum in data:
     ratio.append(float(datum[ar_col]) / float(datum[fe_col]))	#逐列加入Fe/Ar的比值於ratio中
     mse.append(float(datum[mse_col]))
+
 # identify which points the Ar/Fe value is too high. this may indicate the points are cracks in core.
-# leave the points that Ar/Fe value within 2 std of the total Fr/Fe & MSE within 1 std of the total MSE
+# select the data points whose Ar/Fe value is from 0 to mean + 1 std (83.85% population if normal distributed)
+# also the MSE value smaller than 1.5 (practicle experience), and the validity must equal to 1
 
 ratio_std = np.std(ratio)
-mse_std = np.std(mse)
+ratio_mean = np.mean(ratio)
+bottom_delet = -10                  #最後10個資料點(以NK-1的1mm解析度來說就是1公分)可能是掃到膠帶，這是需要調整的
 
-output_file = os.path.expanduser('~/Documents/Python_Training/refined.csv')
-with open(output_file, 'w') as f:
-    #print(column_info, file=f)
-    print(column_info, file=f)
-    for datum in data:
+refined_data = os.path.expanduser('~/Documents/Python_Training/refined_20180913.csv')
+deleted_data = os.path.expanduser('~/Documents/Python_Training/deleted_20180913.csv')
+with open(refined_data, 'w') as f:
+    with open(deleted_data, 'w') as df:
         cur_ratio = float(datum[ar_col])/float(datum[fe_col])
-        if cur_ratio <= ratio_std * 2 and float(datum[mse_col]) <= mse_std:
-            print(','.join(datum), file=f)
+        print(','.join(column_info), file=f)        #用join可以將list中的物件全部以給予的符號連結起來成為一個字串，故具有iteration的特性
+        print(','.join(column_info), file=df)
+        for datum in data[:bottom_delet]:
+            if cur_ratio <= (ratio_std + ratio_mean) and float(datum[mse_col]) <= 1.5 and datum[vld_col] == '1':
+                print(','.join(datum), file=f)  
+                #print(datum, file = f)
+            else:
+                print(','.join(datum), file=df)
+        for datum in data[bottom_delet:]:
+            print(','.join(datum), file=df)
+
+
