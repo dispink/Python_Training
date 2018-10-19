5b81eafdbd751925f412965c46e274ff5dee147c
dispink <r04224110@ntu.edu.tw>
2018-10-19 16:44:27 +0800

    This commit contains the upgrade of "reshow_master.py", input data "Results_NK-1-E_3rd_20151008.xls", compared master result "NKE-1_clr_cor_compile_0430.csv", and output pairs plot "NKE-1_pairs.png" from reshow_master.py.
    The progress of "reshow_master.py":
    1. I copy the needed files to this directory, so I'm much convenient to switch berween mac and windows.
    2. start to use pd.df.plot and seaborn to practice plotting.

--- reshow_master.py
+++ reshow_master.py
@@ -2,22 +2,24 @@
 """
 Created on Thu Oct 18 16:17:58 2018
 Take core NKE-1 as target to re-show my mater thesis using R
+input data: 
+pair data: 
 """
 
 import pandas as pd
 import numpy as np
 from scipy import stats
+import seaborn
 
-# input source excel 
-input_file = "D:/Google drive/Acadamics/Research/Methods_ITRAX/EPA_project_2015/NK-1_W_20151008_3rd/NK-1-W_3rd_xrf_1mm_0-940mm_30kv26ma15sr1/Results_NK-1-W_3rd_20151008.xls"
-df_data = (pd.read_excel(input_file, "Results_NK-1-W_3rd_20151008", skiprows = 2).
+# input source excel            
+df_data = (pd.read_excel('Results_NK-1-W_3rd_20151008.xls', 'Results_NK-1-W_3rd_20151008', skiprows = 2).
            rename(columns = {"position (mm)" : "position_mm"}).
            dropna()
            )
 df_data.head()
 
 # input master result
-df_pair = pd.read_csv('D:/Google drive/Acadamics/Research/Methods_Statistics/2017-04/compile_final_0430/NKE-1_clr_cor_compile_0430.csv')
+df_pair = pd.read_csv('NKE-1_clr_cor_compile_0430.csv')
 df_pair.head()
 
 
@@ -26,24 +28,37 @@ df_pair.head()
 df_data_sel = df_data.loc[ : , ['Si', 'K', 'Ca', 'Ti', 'Cr', 'Br', 'Mn', 'Fe', 'Ni',
                                 'Cu', 'Zn', 'Pb', 'Zr', 'Rb', 'Mo coh']].copy()
 position = df_data['position_mm']
-# replace 0 by 1 since 0 will cause the infinite problem in letter centred log-ratio transformation.
-# 0 reflect the concentration meet the detect limit of Itrax, so use 1 can represent the low count as well and avoid the problem.
-df_data_sel = df_data_sel.replace(0,1)
-df_data_sel.describe().min()
+## replace zero to avoid problems with transform
+#  replace the 0 value with the mean of first 25% observation values in Br & Pb.
+df_data_sel.describe().min()    #only Br and Pb has 0
 
+df_data_sel.Br = df_data_sel.Br.replace( 
+        0, df_data_sel.describe().loc['25%', 'Br']
+        )
+
+df_data_sel.Pb = df_data_sel.Pb.replace( 
+        0, df_data_sel.describe().loc['25%', 'Pb']
+        )
+
+df_data_sel.describe().min()
+# maybe later can use randomnormal number between 1 and 0.1% (-3std) to replace 0
+ 
 # centred log-ratio
 df_clr = pd.DataFrame()
 gm = stats.gmean(df_data_sel, axis = 1)
-for nrow in range(0, len(df_data_sel)):
+for nrow in range(len(df_data_sel)):
     df_clr = df_clr.append(
             np.log(df_data_sel.iloc[nrow]/gm[nrow])
             )
-# why the order of columns is sorted....???
-    
-df_clr['position_mm'] = position
-df_clr['Si'].head()   
-df_pair['Si'].head() 
 
-round(df_clr['Si'] - df_pair['Si'], 3)
-print(df_clr.loc[1, 'Si'], '\n', df_pair.loc[1, 'Si'])
-# some differences should be made by the 0 replacement difference between the Master thesis and here.
+# why the order of columns is re-sorted....???
+    # so I need to manually re-order the columns...
+df_clr = df_clr[['Si', 'K', 'Ca', 'Ti', 'Cr', 'Br', 'Mn', 'Fe', 'Ni',
+                                'Cu', 'Zn', 'Pb', 'Zr', 'Rb', 'Mo coh']]
+
+# eveluate the difference compare to the master result (take Si as representative)
+diff_Si = df_clr['Si'] - df_pair['Si']
+diff_Si.plot('hist')        # they are almost identical
+
+sns_plot = seaborn.pairplot(df_clr)
+sns_plot.savefig('NKE-1_pairs.png')        # it's almost identical to the master result
