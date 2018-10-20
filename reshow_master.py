3673ee1a9c3677e3525b3c00b17286cbef02e985
dispink <r04224110@ntu.edu.tw>
2018-10-20 20:50:53 +0800

    The commit upgrade "reshow_master.py": 1. PCA part (try the mle method bit got a shit... still don't know why...after fancy playing, I go on with the parameter same in master resutl.

--- reshow_master.py
+++ reshow_master.py
@@ -11,64 +11,78 @@ import numpy as np
 from scipy import stats
 import seaborn
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 ## input source excel ##           
-df_data = (pd.read_excel('Results_NK-1-W_3rd_20151008.xls', 'Results_NK-1-W_3rd_20151008', skiprows = 2).
+data_df = (pd.read_excel('Results_NK-1-W_3rd_20151008.xls', 'Results_NK-1-W_3rd_20151008', skiprows = 2).
            rename(columns = {"position (mm)" : "position_mm"}).
            dropna()
            )
-df_data.head()
+data_df.head()
 
 # input master result #
-df_pair = pd.read_csv('NKE-1_clr_cor_compile_0430.csv')
-df_pair.head()
+pair_df = pd.read_csv('NKE-1_clr_cor_compile_0430.csv')
+pair_df.head()
 
 
 # select the elements and position columns
 
-df_data_sel = df_data.loc[ : , ['Si', 'K', 'Ca', 'Ti', 'Cr', 'Br', 'Mn', 'Fe', 'Ni',
+data_sel_df = data_df.loc[ : , ['Si', 'K', 'Ca', 'Ti', 'Cr', 'Br', 'Mn', 'Fe', 'Ni',
                                 'Cu', 'Zn', 'Pb', 'Zr', 'Rb', 'Mo coh']].copy()
-position = df_data['position_mm']
+position = data_df['position_mm']
 
 # replace zero to avoid problems with transform
 #  replace the 0 value with the mean of first 25% observation values in Br & Pb.
-df_data_sel.describe().min()    #only Br and Pb has 0
+data_sel_df.describe().min()    #only Br and Pb has 0
 
-df_data_sel.Br = df_data_sel.Br.replace( 
-        0, df_data_sel.describe().loc['25%', 'Br']
+data_sel_df.Br = data_sel_df.Br.replace( 
+        0, data_sel_df.describe().loc['25%', 'Br']
         )
 
-df_data_sel.Pb = df_data_sel.Pb.replace( 
-        0, df_data_sel.describe().loc['25%', 'Pb']
+data_sel_df.Pb = data_sel_df.Pb.replace( 
+        0, data_sel_df.describe().loc['25%', 'Pb']
         )
 
-df_data_sel.describe().min()
+data_sel_df.describe().min()
 # maybe later can use randomnormal number between 1 and 0.1% (-3std) to replace 0
  
 ## centred log-ratio ##
-df_clr = pd.DataFrame()
-gm = stats.gmean(df_data_sel, axis = 1)
-for nrow in range(len(df_data_sel)):
-    df_clr = df_clr.append(
-            np.log(df_data_sel.iloc[nrow]/gm[nrow])
+clr_df = pd.DataFrame()
+gm = stats.gmean(data_sel_df, axis = 1)
+for nrow in range(len(data_sel_df)):
+    clr_df = clr_df.append(
+            np.log(data_sel_df.iloc[nrow]/gm[nrow])
             )
 
 # why the order of columns is re-sorted....???
     # so I need to manually re-order the columns...
-df_clr = df_clr[['Si', 'K', 'Ca', 'Ti', 'Cr', 'Br', 'Mn', 'Fe', 'Ni',
+clr_df = clr_df[['Si', 'K', 'Ca', 'Ti', 'Cr', 'Br', 'Mn', 'Fe', 'Ni',
                                 'Cu', 'Zn', 'Pb', 'Zr', 'Rb', 'Mo coh']]
 
 # eveluate the difference compare to the master result (take Si as representative)
-diff_Si = df_clr['Si'] - df_pair['Si']
+diff_Si = clr_df['Si'] - pair_df['Si']
 diff_Si.plot('hist')        # they are almost identical
 
-sns_plot = seaborn.pairplot(df_clr)
+sns_plot = seaborn.pairplot(clr_df)
 sns_plot.savefig('NKE-1_pairs.png')        # it's almost identical to the master result
 
-
-
 ## PCA ##
-pca = PCA(n_components = 'mle')
-pca.fit(df_clr)
-print(pca.explained_variance_ratio_) 
+x = StandardScaler().fit_transform(clr_df)   # need to standardized first
+
+pca = PCA(n_components = 'mle', svd_solver = 'full')         # let's try the fancy automatically selection
+PCs = pca.fit_transform(x)
+PCs_df = pd.DataFrame(data = PCs) 
+variance = pca.explained_variance_ratio_
+    
+print('automatically select {} components. \n {}% of variance is retianed.'.format(
+        len(variance), round(variance.sum()*100) )
+    ) 
+print('the auto selection is a shit.... only exclude one component... need to dig more about it..')
+
+pca = PCA( n_components = 3)        # based on my master result
+PCs = pca.fit_transform(x)
+PCs_df = pd.DataFrame(
+        data = PCs,
+        columns = ['PC1', 'PC2', 'PC3'])
+
 
