9ace1a78abff34471df6beae42c11aa9289b40f1
dispink <r04224110@ntu.edu.tw>
2018-10-31 19:01:33 +0800

    start to add the reading of "pore distribution" into the "BET_raw_data_show.py"

--- BET_raw_data_show.py
+++ BET_raw_data_show.py
@@ -11,16 +11,19 @@ os.chdir(path)
 #file = input()
 
 input_name = 'OR-S-B1.txt'
-
+l_start = 0
+pore_distribution = []
 with open(input_name, 'r', errors = 'ignore') as f:      # 有亂碼，直接忽略檔案中的亂碼
-    count = 0
-    #BET_area = ' '
+
     for index, line in enumerate(f):
-        count += 1
-        if line[33 : 49] == 'BET Surface Area':
-            BET_area = line[ 54: 60]
+        #if line[33 : 49] == 'BET Surface Area':
+            #BET_area = line[ 54: 60]
+        if line[0:32] == 'BJH Adsorption dV/dD Pore Volume':
+            l_start = index  + 4
+        if index >= l_start & index <= l_start + 26:
+            pore_distribution.append(line)
 
-print(count)
 
-test = '                                 BET Surface Area'
+test = 'BJH Adsorption dV/dD Pore Volume'
+len(test)
 test[33:49]
